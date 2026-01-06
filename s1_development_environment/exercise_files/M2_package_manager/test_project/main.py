1bb09a6e4470b52fff6245a94c6cf0d34f6ac1d5
MejseGK <s196050@dtu.dk>
2026-01-06 14:37:55 +0100

    Add exercise files for M2, M3 and start of M4 in S1

---
+++ s1_development_environment/exercise_files/M2_package_manager/test_project/main.py
@@ -0,0 +1,4 @@
+import requests
+import numpy as np
+
+print("ok", requests.__version__, np.__version__)
---
+++ s1_development_environment/exercise_files/M3_editor/convert_this.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Hej, det her er en test.
+
+# In[2]:
+
+
+print("2+2 =", 2+2)
+
