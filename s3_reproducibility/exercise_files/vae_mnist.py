aa3d851428756f9fdbacb372bf935adc9417f08a
MejseGK <s196050@dtu.dk>
2026-01-08 08:37:18 +0100

    Do M10 docker exercises

--- s3_reproducibility/exercise_files/vae_mnist.py
+++ s3_reproducibility/exercise_files/vae_mnist.py
@@ -14,28 +14,96 @@ from torch.utils.data import DataLoader
 from torchvision.datasets import MNIST
 from torchvision.utils import save_image
 
-# Model Hyperparameters
-dataset_path = "~/datasets"
-cuda = torch.cuda.is_available()
-DEVICE = torch.device("cuda" if cuda else "cpu")
-batch_size = 100
-x_dim = 784
-hidden_dim = 400
+import hydra
+from omegaconf import DictConfig
+from hydra.core.hydra_config import HydraConfig
+import logging
 
-# Data loading
-mnist_transform = transforms.Compose([transforms.ToTensor()])
 
-train_dataset = MNIST(dataset_path, transform=mnist_transform, train=True, download=True)
-test_dataset = MNIST(dataset_path, transform=mnist_transform, train=False, download=True)
+@hydra.main(config_name="config.yaml", config_path=".", version_base=None)
+def main(cfg: DictConfig):
 
-train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
-test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+    # Configuration settings
+    dataset_path = "~/datasets"
+    cuda = torch.cuda.is_available()
+    DEVICE = torch.device("cuda" if cuda else "cpu")
+    log = logging.getLogger(__name__)
+    out_dir = HydraConfig.get().runtime.output_dir
 
-encoder = Encoder(input_dim=x_dim, hidden_dim=hidden_dim, latent_dim=20)
-decoder = Decoder(latent_dim=20, hidden_dim=hidden_dim, output_dim=x_dim)
+    # Model Hyperparameters
+    torch.manual_seed(cfg.hyperparameters.seed)
+    batch_size = cfg.hyperparameters.batch_size
+    x_dim = cfg.hyperparameters.x_dim
+    hidden_dim = cfg.hyperparameters.hidden_dim
+    latent_dim = cfg.hyperparameters.latent_dim
+    lr = cfg.hyperparameters.lr
+    epochs = cfg.hyperparameters.epochs
 
-model = Model(encoder=encoder, decoder=decoder).to(DEVICE)
+    # Data loading
+    mnist_transform = transforms.Compose([transforms.ToTensor()])
 
+    train_dataset = MNIST(dataset_path, transform=mnist_transform, train=True, download=True)
+    test_dataset = MNIST(dataset_path, transform=mnist_transform, train=False, download=True)
+
+    train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+
+    # Build model
+    encoder = Encoder(input_dim=x_dim, hidden_dim=hidden_dim, latent_dim=latent_dim)
+    decoder = Decoder(latent_dim=latent_dim, hidden_dim=hidden_dim, output_dim=x_dim)
+
+    model = Model(encoder=encoder, decoder=decoder).to(DEVICE)
+
+    # Optimizer
+    optimizer = Adam(model.parameters(), lr=lr)
+
+    # Training model
+    log.info("Start training VAE...")
+    model.train()
+    for epoch in range(epochs):
+        overall_loss = 0
+        for batch_idx, (x, _) in enumerate(train_loader):
+            if batch_idx % 100 == 0:
+                log.info(batch_idx)
+            x = x.view(batch_size, x_dim)
+            x = x.to(DEVICE)
+
+            optimizer.zero_grad()
+
+            x_hat, mean, log_var = model(x)
+            loss = loss_function(x, x_hat, mean, log_var)
+
+            overall_loss += loss.item()
+
+            loss.backward()
+            optimizer.step()
+        log.info(f"Epoch {epoch + 1} complete!,  Average Loss: {overall_loss / (batch_idx * batch_size)}")
+    log.info("Finish!!")
+
+    # save weights
+    #torch.save(model, os.path.join(out_dir, "trained_model.pt")) # saves entire model
+    torch.save(model.state_dict(), os.path.join(out_dir, "trained_model.pt")) # only saves model weights
+
+    # Generate reconstructions
+    model.eval()
+    with torch.no_grad():
+        for batch_idx, (x, _) in enumerate(test_loader):
+            if batch_idx % 100 == 0:
+                log.info(batch_idx)
+            x = x.view(batch_size, x_dim)
+            x = x.to(DEVICE)
+            x_hat, _, _ = model(x)
+            break
+
+    save_image(x.view(batch_size, 1, 28, 28), os.path.join(out_dir, "orig_data.png"))
+    save_image(x_hat.view(batch_size, 1, 28, 28), os.path.join(out_dir, "reconstructions.png"))
+
+    # Generate samples
+    with torch.no_grad():
+        noise = torch.randn(batch_size, 20).to(DEVICE)
+        generated_images = decoder(noise)
+
+    save_image(generated_images.view(batch_size, 1, 28, 28), os.path.join(out_dir, "generated_sample.png"))
 
 def loss_function(x, x_hat, mean, log_var):
     """Elbo loss function."""
@@ -43,52 +111,5 @@ def loss_function(x, x_hat, mean, log_var):
     kld = -0.5 * torch.sum(1 + log_var - mean.pow(2) - log_var.exp())
     return reproduction_loss + kld
 
-
-optimizer = Adam(model.parameters(), lr=1e-3)
-
-
-print("Start training VAE...")
-model.train()
-for epoch in range(20):
-    overall_loss = 0
-    for batch_idx, (x, _) in enumerate(train_loader):
-        if batch_idx % 100 == 0:
-            print(batch_idx)
-        x = x.view(batch_size, x_dim)
-        x = x.to(DEVICE)
-
-        optimizer.zero_grad()
-
-        x_hat, mean, log_var = model(x)
-        loss = loss_function(x, x_hat, mean, log_var)
-
-        overall_loss += loss.item()
-
-        loss.backward()
-        optimizer.step()
-    print(f"Epoch {epoch + 1} complete!,  Average Loss: {overall_loss / (batch_idx * batch_size)}")
-print("Finish!!")
-
-# save weights
-torch.save(model, f"{os.getcwd()}/trained_model.pt")
-
-# Generate reconstructions
-model.eval()
-with torch.no_grad():
-    for batch_idx, (x, _) in enumerate(test_loader):
-        if batch_idx % 100 == 0:
-            print(batch_idx)
-        x = x.view(batch_size, x_dim)
-        x = x.to(DEVICE)
-        x_hat, _, _ = model(x)
-        break
-
-save_image(x.view(batch_size, 1, 28, 28), "orig_data.png")
-save_image(x_hat.view(batch_size, 1, 28, 28), "reconstructions.png")
-
-# Generate samples
-with torch.no_grad():
-    noise = torch.randn(batch_size, 20).to(DEVICE)
-    generated_images = decoder(noise)
-
-save_image(generated_images.view(batch_size, 1, 28, 28), "generated_sample.png")
+if __name__ == "__main__":
+    main()
