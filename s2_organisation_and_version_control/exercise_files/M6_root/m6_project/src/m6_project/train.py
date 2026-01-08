8efb789dd64b5042edcfbd5239a4042b72f88ffd
MejseGK <s196050@dtu.dk>
2026-01-08 20:22:39 +0100

    Do M14 Logging

--- s2_organisation_and_version_control/exercise_files/M6_root/m6_project/src/m6_project/train.py
+++ s2_organisation_and_version_control/exercise_files/M6_root/m6_project/src/m6_project/train.py
@@ -1,17 +1,29 @@
+from asyncio import run
 import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import torch
+import typer
+import wandb
+import argparse
 from m6_project.model import MyAwesomeModel
 from m6_project.data import corrupt_mnist
+from sklearn.metrics import RocCurveDisplay, accuracy_score, f1_score, precision_score, recall_score
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
 
-
-def train(lr: float = 1e-3, batch_size: int = 32, epochs: int = 10) -> None:
+def train(lr: float = 1e-3, batch_size: int = 32, epochs: int = 5) -> None:
     """Train a model on MNIST."""
     print("Training day and night")
     print(f"{lr=}, {batch_size=}, {epochs=}")
+
+    # Start a new wandb run to track this script.
+    run = wandb.init(
+        entity="mejsegk-technical-university-of-denmark",
+        project="m6_project",
+        job_type="train",
+        config={"lr": lr, "batch_size": batch_size, "epochs": epochs},
+    )
 
     model = MyAwesomeModel().to(DEVICE)
     train_set, _ = corrupt_mnist()
@@ -20,10 +32,12 @@ def train(lr: float = 1e-3, batch_size: int = 32, epochs: int = 10) -> None:
 
     loss_fn = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-
+    
     statistics = {"train_loss": [], "train_accuracy": []}
     for epoch in range(epochs):
         model.train()
+
+        preds, targets = [], []
         for i, (img, target) in enumerate(train_dataloader):
             img, target = img.to(DEVICE), target.to(DEVICE)
             optimizer.zero_grad()
@@ -35,12 +49,42 @@ def train(lr: float = 1e-3, batch_size: int = 32, epochs: int = 10) -> None:
 
             accuracy = (y_pred.argmax(dim=1) == target).float().mean().item()
             statistics["train_accuracy"].append(accuracy)
+            wandb.log({"train_loss": loss.item(), "train_accuracy": accuracy})
+
+            preds.append(y_pred.detach().cpu())
+            targets.append(target.detach().cpu())
 
             if i % 100 == 0:
                 print(f"Epoch {epoch}, iter {i}, loss: {loss.item()}")
 
+                # add a plot of one input image
+                image = wandb.Image(img[0].detach().cpu(), caption="Input image")
+                wandb.log({"image": image})
+
+                # add a plot of histogram of the gradients
+                grads = torch.cat([p.grad.flatten() for p in model.parameters() if p.grad is not None], 0)
+                wandb.log({"gradients": wandb.Histogram(grads)})
+        
+         # add a custom matplotlib plot of the ROC curves
+        preds = torch.cat(preds, 0)
+        targets = torch.cat(targets, 0)
+
+        for class_id in range(10):
+            one_hot = torch.zeros_like(targets)
+            one_hot[targets == class_id] = 1
+            _ = RocCurveDisplay.from_predictions(
+                one_hot,
+                preds[:, class_id],
+                name=f"ROC curve for {class_id}",
+                plot_chance_level=(class_id == 2),
+            )
+
+        wandb.log({"roc": wandb.Image(plt)})
+        plt.close()  # close the plot to avoid memory leaks and overlapping figures
+
+    # saved locally, not on wandb
     print("Training complete")
-    torch.save(model.state_dict(), "models/model.pth")
+    torch.save(model.state_dict(), "models/model.pth") # save model locally
     fig, axs = plt.subplots(1, 2, figsize=(15, 5))
     axs[0].plot(statistics["train_loss"])
     axs[0].set_title("Train loss")
@@ -48,5 +92,39 @@ def train(lr: float = 1e-3, batch_size: int = 32, epochs: int = 10) -> None:
     axs[1].set_title("Train accuracy")
     fig.savefig("reports/figures/training_statistics.png")
 
+    # To log in wandb
+    final_accuracy = accuracy_score(targets, preds.argmax(dim=1))
+    final_precision = precision_score(targets, preds.argmax(dim=1), average="weighted")
+    final_recall = recall_score(targets, preds.argmax(dim=1), average="weighted")
+    final_f1 = f1_score(targets, preds.argmax(dim=1), average="weighted")
+
+    # log model as an artifact
+    artifact = wandb.Artifact(
+        name="m6_project_model",
+        type="model",
+        description="A model trained to classify corrupt MNIST images",
+        metadata={"accuracy": final_accuracy, "precision": final_precision, "recall": final_recall, "f1": final_f1},
+    )
+    artifact.add_file("models/model.pth")
+    run.log_artifact(artifact)
+
+    # Link artifact to a model registry
+    run.link_artifact(
+        artifact=artifact,
+        target_path="model-registry/corrupt_mnist",
+        aliases=["latest"]
+    )
+
+    # Finish the run and upload any remaining data.
+    run.finish()
+
 if __name__ == "__main__":
     train()
+
+# Set up argument parser
+parser = argparse.ArgumentParser()
+# Arguments that can be passed from command line
+parser.add_argument("--lr", type=float, default=1e-3)
+parser.add_argument("--epochs", type=int, default=10)
+parser.add_argument("--batch-size", type=int, default=32)
+args = parser.parse_args()
